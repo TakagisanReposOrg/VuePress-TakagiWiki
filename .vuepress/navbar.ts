667a1bfd41268a23139617d5c54ec49de9230f58
一只鬆 <yzsong06@outlook.com>
2023-06-05 10:29:30 +0800

    Update navbar.ts

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -8,19 +8,63 @@ export default navbar([
     children: [
       {
         text: "动画片尾曲",
-        link: "music/ED.md",
+        link: "docs/music/ED.md",
       },
       {
         text: "动画主题曲",
-        link: "music/OP.md",
+        link: "docs/music/OP.md",
       },
       {
         text: "原声带OST",
-        link: "music/OST.md",
+        link: "docs/music/OST.md",
       },
       {
         text: "手游 “心动记录”",
-        link: "music/ED.md",
+        link: "docs/music/ED.md",
+      },
+        ],
+  },
+  {
+    text: "漫画",
+    prefix: "docs/manga/",
+    children: [
+      {
+        text: "高木同学正篇",
+        link: "docs/manga/Takagi-OG.md",
+      },
+      {
+        text: "原高木同学",
+        link: "docs/manga/Spinoff-Moto.md",
+      },
+      {
+        text: "第三季",
+        link: "docs/anime/S3.md",
+      },
+      {
+        text: "剧场版",
+        link: "docs/anime/Movie.md",
+      },
+        ],
+  },
+  {
+    text: "动画",
+    prefix: "docs/anime/",
+    children: [
+      {
+        text: "第一季",
+        link: "docs/anime/S1.md",
+      },
+      {
+        text: "第二季",
+        link: "docs/anime/S2.md",
+      },
+      {
+        text: "第三季",
+        link: "docs/anime/S3.md",
+      },
+      {
+        text: "剧场版",
+        link: "docs/anime/Movie.md",
       },
         ],
   },
