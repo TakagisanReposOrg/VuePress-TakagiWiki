72731f84f7b8721baffa06e02f7a2bb9568ed359
一只鬆 <yzsong06@outlook.com>
2023-06-05 13:50:48 +0800

    Update sidebar.ts

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -1,21 +1,47 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
+     [
     "",
     {
-      text: "案例",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
+      text: "音乐",
+      prefix: "/docs/music/",
+      children: [
+          "ED.md" ,
+          "OP.md" ,
+          "OST.md" ,
+          "GAME.md" ,
+        ],
     },
     {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
+      text: "漫画",
+      prefix: "/docs/manga/",
+      children: [
+          "Takagi-OG.md" ,
+          "Spinoff-Moto.md" ,
+          "Spinoff-Tis.md" ,
+          "Spinoff-Yukari.md" ,
+        ],
     },
-    "slides",
+    {
+      text: "动画",
+      prefix: "/docs/anime/",
+      children: [
+          "S1.md" ,
+          "S2.md" ,
+          "S3.md" ,
+          "Movie.md" ,
+        ],
+    },
+    {
+      text: "衍生/社群",
+      prefix: "/docs/derivative/",
+      children: [
+          "Related-Book.md" ,
+          "Related-Software.md" ,
+          "Related-Creation.md" ,
+          "FanGroup.md" ,
+        ],
+    },
   ],
 });
