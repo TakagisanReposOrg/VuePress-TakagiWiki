53f5a71d6481f76792e4046369ef0f341e07582e
一只鬆 <yzsong06@outlook.com>
2023-06-05 03:08:37 +0000

    Compleate

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,70 +1,70 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
+
   {
     text: "音乐",
-    prefix: "/music/",
+    prefix: "/docs/music/",
     children: [
       {
         text: "动画片尾曲",
-        link: "docs/music/ED.md",
+        link: "ED.md",
       },
       {
         text: "动画主题曲",
-        link: "docs/music/OP.md",
+        link: "OP.md",
       },
       {
         text: "原声带OST",
-        link: "docs/music/OST.md",
+        link: "OST.md",
       },
       {
         text: "手游 “心动记录”",
-        link: "docs/music/ED.md",
+        link: "ED.md",
       },
         ],
   },
   {
     text: "漫画",
-    prefix: "docs/manga/",
+    prefix: "/docs/manga/",
     children: [
       {
         text: "高木同学正篇",
-        link: "docs/manga/Takagi-OG.md",
+        link: "Takagi-OG.md",
       },
       {
         text: "原高木同学",
-        link: "docs/manga/Spinoff-Moto.md",
+        link: "Spinoff-Moto.md",
       },
       {
-        text: "第三季",
-        link: "docs/anime/S3.md",
+        text: "明天是星期六",
+        link: "Spinoff-Tis.md",
       },
       {
-        text: "剧场版",
-        link: "docs/anime/Movie.md",
+        text: "与恋爱相恋的友加里",
+        link: "Spinoff-Yukari.md",
       },
         ],
   },
   {
     text: "动画",
-    prefix: "docs/anime/",
+    prefix: "/docs/anime/",
     children: [
       {
         text: "第一季",
-        link: "docs/anime/S1.md",
+        link: "S1.md",
       },
       {
         text: "第二季",
-        link: "docs/anime/S2.md",
+        link: "S2.md",
       },
       {
         text: "第三季",
-        link: "docs/anime/S3.md",
+        link: "S3.md",
       },
       {
         text: "剧场版",
-        link: "docs/anime/Movie.md",
+        link: "Movie.md",
       },
         ],
   },
