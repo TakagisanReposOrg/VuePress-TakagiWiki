c934b78cb72b6c10a633e7927dad46a057c0712c
一只鬆 <yzsong06@outlook.com>
2023-06-08 00:10:18 +0000

    搜索功能本地语言化（zh-CN）

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -14,9 +14,18 @@ export default defineUserConfig({
     docsearchPlugin({
       indexName: "takagi",
       appId: "OX0S0VN0WW",
-      apiKey: "6c608654315e32aa4752540754d0d525"
+      apiKey: "6c608654315e32aa4752540754d0d525",
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+            },
+          },
+        },
       // appId, apiKey 和 indexName 是必填的
-    }),
+    }}),
   ],
 
   // Enable it with pwa
